243ce0b0465e7ce709e571deccc1a4bc8ef03000
AninhaPardini <aninhapardini.work@gmail.com>
2023-06-14 21:08:59 -0300

    🌐 Implementando hook para conferir rotas

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,11 @@ import cookie from '@fastify/cookie'
 const app = fastify()
 
 app.register(cookie)
+
+app.addHook('preHandler', async (request, reply) => {
+  console.log(`[${request.method}] ${request.url}`)
+})
+
 app.register(transactionsRoutes, {
   prefix: 'transactions',
 })
