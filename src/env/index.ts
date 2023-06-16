330e5e23c25fe7a513725270abe05061225f6c2a
AninhaPardini <aninhapardini.work@gmail.com>
2023-06-15 21:43:01 -0300

    ✅ Pronto para deploy

--- src/env/index.ts
+++ src/env/index.ts
@@ -11,8 +11,9 @@ console.log(process.env.NODE_ENV)
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3000),
+  PORT: z.coerce.number().default(3000),
 })
 
 const _env = envSchema.safeParse(process.env)
